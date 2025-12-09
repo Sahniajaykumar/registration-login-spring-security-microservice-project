13bc537bca66b290d6e53f90ec1bc4add3707fcf
Ajay Kumar <your-email@example.com>
2025-12-09 23:27:53 +0530

    Initial microservice upload

---
+++ auth-service/src/main/java/com/auth_service/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.auth_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.auth_service.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUsername(String username);
+	User findByEmail(String email);
+	boolean existsByUsername(String username);
+	boolean existsByEmail(String email);
+}
